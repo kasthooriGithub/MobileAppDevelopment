2439186e810f871de77ad826ce6ae612dccbdff1
kasthooriGithub <kasthoorikanesh73@gmail.com>
2025-08-25 14:52:27 +0530

    Day 6 - React setup + basic screens + Firebase init

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+function About() { 
+return ( 
+<div className="container mt-5"> 
+<h2>About {`mobile_app_dev`}</h2> 
+<p>This app is developed to demonstrate mobile app skills using React and Firebase.</p> 
+</div> 
+); 
+} 
+export default About;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDicme-wQh8D0Gdh-EaypEh4yXr2pUR4W8",
+  authDomain: "mobileappdev-1ef40.firebaseapp.com",
+  projectId: "mobileappdev-1ef40",
+  storageBucket: "mobileappdev-1ef40.firebasestorage.app",
+  messagingSenderId: "276934337316",
+  appId: "1:276934337316:web:1f46b87bf0fefeaaf709b1",
+  
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log("Firebase initialized", app);
+
+export default app;
